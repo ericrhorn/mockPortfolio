792ffe982a1fcaae7ad7b53772ea7653656678b4
Eric Horn <hornericr@me.com>
2023-10-16 16:56:51 -0700

    commit test

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -9,7 +9,7 @@ import { useAlertContext } from "../context/alertContext";
 import { useRef } from "react";
 
 /**
- * This is a global component that uses context to display a global alert message. test for git
+ * This is a global component that uses context to display a global alert message. commit test for git 
  */
 function Alert() {
   const { isOpen, type, message, onClose } = useAlertContext();
